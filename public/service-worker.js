314cb50ca2470888c2f74d2166e5f612452bf671
Lilyokalani Hafoka <lilyhafoka.du@gmail.com>
2021-02-12 09:25:17 -0700

    updating service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,18 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/public/style.css",
-  "/public/db.js",
-  "https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap",
-  "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css",
-]
+  "/styles.css",
+  "/index.js",
+  "/db.js",
+  "/manifest.webmanifest",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+];
 
 const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
