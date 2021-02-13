001b68e0aaafb761a0e5dd42aea406a98d8a9c17
Lilyokalani Hafoka <lilyhafoka.du@gmail.com>
2021-02-12 17:29:47 -0700

    updating server.js

--- server.js
+++ server.js
@@ -15,12 +15,22 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
 });
+} else {
+  mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+}
 
 // routes
 app.use(require("./routes/api.js"));
